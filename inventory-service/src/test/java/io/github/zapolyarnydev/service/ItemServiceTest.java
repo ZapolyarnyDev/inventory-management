c28842cacdb723eb148ab01dbffc19f733c187f6
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-06-17 18:43:56 +0300

    Adding first tests for inventory service

---
+++ inventory-service/src/test/java/io/github/zapolyarnydev/service/ItemServiceTest.java
@@ -0,0 +1,55 @@
+package io.github.zapolyarnydev.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Тестирование сервиса работающего с товарами на складе")
+public class ItemServiceTest {
+
+    @Mock
+    private InventoryRepository inventoryRepository;
+
+    @InjectMocks
+    private InventoryService inventoryService;
+
+    @Captor
+    private ArgumentCaptor<InventoryItemEntity> itemCaptor;
+
+    @DisplayName("Сервис должен сохранить предмет Book на склад")
+    @Test
+    public void shouldServiceSaveItemToInventoryRepository() {
+        var itemEntity = new InventoryItemEntity("Book", 30);
+
+        inventoryService.saveItem(itemEntity);
+
+        verify(inventoryRepository).save(itemCaptor.capture());
+
+        assertEquals(itemEntity.getName(), itemCaptor.getValue().getName());
+    }
+
+    @DisplayName("Сервис должен удалить предмет Book со склада")
+    @Test
+    public void shouldServiceRemoveItemFromInventoryRepository() {
+        var itemEntity = new InventoryItemEntity("Book", 30);
+
+        inventoryService.removeItem(itemEntity);
+
+        verify(inventoryRepository).delete(itemCaptor.capture());
+
+        assertEquals(itemEntity.getName(), itemCaptor.getValue().getName());
+    }
+}
