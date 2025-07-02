1d94c6a06547e4990e5bc4546754671530e68c1f
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-07-02 21:22:04 +0300

    fix: kafka consumers receive events correctly

--- order-service/src/main/java/io/github/zapolyarnydev/controller/OrderController.java
+++ order-service/src/main/java/io/github/zapolyarnydev/controller/OrderController.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/orders")
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderPlaceService orderPlaceService;
